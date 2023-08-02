827a13d2dc03be2eb709edb96a39745cf49f0c3b
liyou <1282261308@qq.com>
2023-08-02 15:35:16 +0800

    AOP IOC

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Inject, Param } from '@nestjs/common';
 import { get } from 'http';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService,
+    @Inject('person2') private readonly person2: { name: string, desc: string }
+  ) { }
+
 
   @Get()
   getHello(): string {
+    debugger
+    console.log('this.person2', this.person2)
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonModule } from './person/person.module';
+import { AaaModule } from './aaa/aaa.module';
+import { BbbModule } from './bbb/bbb.module';
+import { CccModule } from './ccc/ccc.module';
+import { DddModule } from './ddd/ddd.module';
 
 @Module({
-  imports: [PersonModule],
+  imports: [PersonModule, AaaModule, BbbModule, CccModule, DddModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: 'person2',
+    useFactory() {
+      return {
+        name: 'bbb',
+        desc: 'cccc'
+      }
+    }
+  }],
 })
-export class AppModule {}
+export class AppModule { }
